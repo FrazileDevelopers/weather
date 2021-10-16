290e57d69320c96666fa5e55a01a67817d0c05a7
Parth <frazilex@gmail.com>
2021-10-16 11:42:38 +0530

    abstract class updated

--- lib/data/model/weather.dart
+++ lib/data/model/weather.dart
@@ -8,4 +8,16 @@ class Weather {
   });
 
   // == and hashCode overrides...
+
+  @override
+  bool operator ==(Object o) {
+    if (identical(this, o)) return true;
+
+    return o is Weather &&
+        o.cityName == cityName &&
+        o.temperatureCelsius == temperatureCelsius;
+  }
+
+  @override
+  int get hashCode => cityName.hashCode ^ temperatureCelsius.hashCode;
 }
